77b867b927604a484da429b74def9600d80a3ea6
Vega <NVEGA@emory.edu>
2022-06-15 14:55:56 -0400

    Added comments to code

--- sim_means_4dist.R
+++ sim_means_4dist.R
@@ -5,16 +5,18 @@ sim_means_4dist<-function(n_reps, sample_sizes, shared_mean){
   # n_reps is the number of "samples" to generate
   # sample_sizes is a vector of sample sizes
   # shared_mean is the population mean for all three distributions
-  # Returns a tibble where observed mean values are
+  # Returns a tibble where observed mean values are stored for each "sample"
   
   # Create an empty tibble with three named columns
   distribution_means<-tibble(dist_name=character(),
                              sample_size=numeric(),
                              means=numeric())
   # and populate it
-  i<-1
+  # Iterate over each of the values in the vector of sample sizes
   for (i in 1:length(sample_sizes)){
+    # Within each sample size, execute the indicated number of replicates
     for (j in 1:n_reps){
+      # For each replicate, update the distribution_means object with the new data
       distribution_means<-rbind(distribution_means,
                                 tibble(
                                   dist_name=c("Norm", "LNorm", "Pois", "Exp"),
--- sim_means_4dist_fast.R
+++ sim_means_4dist_fast.R
@@ -5,17 +5,22 @@ sim_means_4dist_fast<-function(n_reps, sample_sizes, shared_mean){
   # n_reps is the number of "samples" to generate
   # sample_sizes is a vector of sample sizes
   # shared_mean is the population mean for all three distributions
-  # Returns a tibble where observed mean values are
+  # Returns a tibble where observed mean values are stored for each "sample"
   
-  # Create an empty tibble with three named columns
+  # Create an empty temporary vector of defined size to hold the data
   temp<-vector("list", length=length(sample_sizes)*n_reps)
     
   # and populate it
+  # Establish an index which will be used to store data
   idx<-1
+  # Iterate over each of the values in the vector of sample sizes
   for (i in 1:length(sample_sizes)){
-    #print(i)
+    #print(i) # for debugging
+    # Within each sample size, execute the indicated number of replicates
     for (j in 1:n_reps){
-      #print(j)
+      #print(j) # for debugging
+      # Within each replicate, create a tibble with the mean from each sample
+      # and staple it into the temporary vector
       temp[[idx]]<-tibble(dist_name=c("Norm", "LNorm", "Pois", "Exp"),
                           sample_size=sample_sizes[i],
                           means=c(mean(rnorm(sample_sizes[i], mean=shared_mean, sd=shared_mean)),
@@ -28,6 +33,8 @@ sim_means_4dist_fast<-function(n_reps, sample_sizes, shared_mean){
       idx<-idx+1
     }
   }
+  # Rearrange the finished data from temp into a proper tibble
+  # with columns dist_name, sample_size, and means
   distribution_means<-dplyr::bind_rows(temp)
   return(distribution_means)
 }
