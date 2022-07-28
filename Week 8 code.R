600596a3f41969658429920fa1c15ce027ef2b07
Vega <NVEGA@emory.edu>
2022-07-28 10:22:34 -0400

    Week 8 bad analysis code from Zee

--- Week 8 code.R
+++ Week 8 code.R
@@ -1,10 +1,16 @@
 pacman::p_load(tidyverse, ggpubr, readxl,ggdark,plotrix,ggpubr)
 
-"ICC_data_df" <-read_excel("ICC_data.xlsx")
+# Read in the data file
+"ICC_data_df" <-read_excel("ICC_data_Week8.xlsx")
 
+# Take a peek
+glimpse(ICC_data_df)
+
+# What are we doing here?
 sixtymin_df <- ICC_data_df %>% 
   filter(trial==1) %>% 
   filter(treatment == "veh60" | treatment == "mbcd60")
+glimpse(sixtymin_df)
   
 #Summary Stats
 summary_df <-sixtymin_df %>% 
@@ -14,13 +20,15 @@ summary_df <-sixtymin_df %>%
     mean = mean(relative_ratio),
     sd = sd(relative_ratio)
   )
-
+summary_df
 
 #Plot it out
 sixtymin_df %>% 
   ggplot(mapping = aes(treatment,relative_ratio))+
   geom_violin(fill = '#FDC314')+
   dark_mode(theme_classic())+
+  theme(text=element_text(size=18, color="white"),
+        axis.text = element_text(size=18, color="white"))+
   labs(x = "Time (min)", #Adds the labels
        y = "Surface/Internal mGluR2")
 
