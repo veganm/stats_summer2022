8cc71d00ba0d7c34084c1c474a3e629ea129d134
Vega <NVEGA@emory.edu>
2022-07-28 13:30:34 -0400

    Week 9 script, preliminary version with helper code

---
+++ VegaSummer2022Week9_Premeeting.R
@@ -0,0 +1,23 @@
+# Vega Lab Meeting Week 9
+# Pre-meeting script, with starter code
+#
+# First we load the required packages
+pacman::p_load(tidyverse, effectsize, pwr)
+
+# Then we take (or make) some data
+# I am going to make some up
+set.seed(1234)
+my_data <- data.frame(
+  group = rep(c("A", "B"), each=10), 
+  data = c(round(rnorm(10, 20, 2), 1), round(rnorm(10, 22, 2), 1))
+)
+glimpse(my_data)
+
+# let's say I want to do a t-test.
+# First I want to know the effect size:
+TT<-t.test(data~group, data=my_data, var.equal=TRUE)
+TT
+effectsize(TT)
+
+# and then the power:
+pwr.t.test(d=-0.8, n=10, sig.level = 0.05, alternative="two.sided")
