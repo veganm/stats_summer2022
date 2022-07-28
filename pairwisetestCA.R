b247839e99c4dcdb62ac1f54df12f8450cb196b8
Vega <NVEGA@emory.edu>
2022-07-28 10:17:53 -0400

    Week 8 bad data analysis from Carmen

--- pairwisetestCA.R
+++ pairwisetestCA.R
@@ -1,7 +1,17 @@
 #Import dataset 
 library(readxl)
+library(tidyverse)
 worms <- read_excel("fakeworms.xlsx")
 View(worms)
+
+# Nic added:
+# Visualize the data
+worms %>%
+  ggplot(aes(x=Time, y=CFU, color=Time))+
+  geom_jitter(width=0.1, size=2)+
+  scale_y_log10()+
+  theme_classic()+
+  theme(text=element_text(size=16))
 
 ##Shapiro-Wilk normality test for the differences
 ##
