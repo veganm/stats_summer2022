1b20fe6db5fbd293d4d1a4c104518e2b3af40fd1
Vega <NVEGA@emory.edu>
2022-08-05 13:01:33 -0400

    Week 10 script, draft

--- VegaSummer2022Week10.R
+++ VegaSummer2022Week10.R
@@ -16,8 +16,7 @@ pacman::p_load(tidyverse, effectsize, pwr, cowplot, ggpubr)
 # Recall back in week 5, when we learned to generate and plot out a normal distribution.
 # We'll start with that code to learn how a Z test is constructed.
 
-# We define a shared mean for all data
-shared_mean<-5 # our data will have the same true mean
+shared_mean<-5 # our data will have the indicated mean
 shared_sd<-2 # and variance
 
 # For the normal, start by creating a sequence of values based on population mean and standard deviation
@@ -28,14 +27,44 @@ c(-4, 4)*shared_sd+shared_mean # what's happening here? hmm.
 # then generate & draw simulated data from the normal distribution using 
 ?stat_function()
 
-pNorm<-
+pNormal<-
   ggplot(data.frame(x = c(-4, 4)*shared_sd+shared_mean), aes(x = x)) +
   stat_function(fun = dnorm, args=list(mean=shared_mean, sd=shared_sd))+
   theme_classic()+
   theme(text=element_text(size=16), 
         plot.title=element_text(hjust=0.5, size=16)) + 
   labs(title="Normal")
-pNorm
+pNormal
+
+# The function pnorm() gives the distribution function for a normal.
+# Let's see what this means. 
+# Note that lower.tail is a logical; 
+# if TRUE (default), probabilities are P[X ≤ x] otherwise, P[X > x].
+pnorm(5, mean=shared_mean, sd=shared_sd)
+pnorm(5, mean=shared_mean, sd=shared_sd, lower.tail = FALSE)
+qnorm(0.5, mean=shared_mean, sd=shared_sd)
+qnorm(0.5, mean=shared_mean, sd=shared_sd, lower.tail = FALSE)
+
+x1<-qnorm(0.025, mean=shared_mean, sd=shared_sd)
+x1
+x2<-qnorm(0.025, mean=shared_mean, sd=shared_sd, lower.tail = FALSE)
+x2
+pnorm(x1, mean=shared_mean, sd=shared_sd)
+pnorm(x2, mean=shared_mean, sd=shared_sd, lower.tail = FALSE)
+
+qnorm(0.25, mean=shared_mean, sd=shared_sd)
+qnorm(0.75, mean=shared_mean, sd=shared_sd)
+
+pnorm(shared_mean-shared_sd, mean=shared_mean, sd=shared_sd)
+pnorm(shared_mean+shared_sd, mean=shared_mean, sd=shared_sd)
+pnorm(shared_mean+0.5*shared_sd, mean=shared_mean, sd=shared_sd) - pnorm(shared_mean-0.5*shared_sd, mean=shared_mean, sd=shared_sd)
+pnorm(shared_mean+1*shared_sd, mean=shared_mean, sd=shared_sd) - pnorm(shared_mean-1*shared_sd, mean=shared_mean, sd=shared_sd)
+pnorm(shared_mean+1.5*shared_sd, mean=shared_mean, sd=shared_sd) - pnorm(shared_mean-1.5*shared_sd, mean=shared_mean, sd=shared_sd)
+
+# Let's visualize:
+pNormal + 
+  geom_vline(xintercept=x1, color="red") +
+  geom_vline(xintercept=x2, color="red")
 
 # We've glossed over something interesting.
 # We rescaled the interval in a very particular way using the shared mean and SD.
